03752ec618b7174e8a8c23c7c657d7fe5bfc2076
Sam Richardson <samrichardson@GJ62-Sam-Richardson.local>
2019-10-09 17:00:56 -0700

    remove api, it is in a separate repo now

--- api/libs/dynamodb-lib.js
+++
@@ -1,9 +0,0 @@
-import AWS from "aws-sdk";
-
-AWS.config.update({ region: "us-west-2" });
-
-export function call(action, params) {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-  return dynamoDb[action](params).promise();
-}
